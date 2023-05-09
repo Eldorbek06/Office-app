bf08e04acfd92ac249472d7172beb8c62c825e03
Eldor06 <eldorergashev20sm>
2023-05-09 19:08:33 +0500

    not completed ts

--- src/modules/ui.ts
+++ src/modules/ui.ts
@@ -1,16 +1,65 @@
 export function reload(arr: Array<object>, place: HTMLElement) {
     place.innerHTML = ''
     for (let item of arr) {
-        place.innerHTML += `
-        <div class="main__item">
-            <p class="main__name" id="employee-name">${item.name}</p>
-            <input class="main__inp" type="number" value="${item.salary}">
-            <div class="main__actions">
-                <img class="main__icon" src="/public/icons/cookie.png" alt="icon">
-                <img class="main__icon" src="/public/icons/bin.png" alt="icon">
-                <img class="main__icon" id="star" src="/public/icons/star.png" alt="icon">
-            </div>
-        </div>
-        `
+        let main__item: HTMLElement = document.createElement('div')
+        let name: HTMLElement = document.createElement('p')
+        let inp: HTMLInputElement = document.createElement('input')
+        let actions: HTMLElement = document.createElement('div')
+        let iconOne: HTMLImageElement = document.createElement('img')
+        let iconTwo: HTMLImageElement = document.createElement('img')
+        let iconThree: HTMLImageElement = document.createElement('img')
+
+        main__item.classList.add('main__item')
+        name.classList.add('main__name')
+        inp.classList.add('main__inp')
+        actions.classList.add('main__actions')
+        iconOne.classList.add('main__icon')
+        iconTwo.classList.add('main__icon')
+        iconThree.classList.add('main__icon-star')
+        if (item.increase) {
+            main__item.classList.add('main__item_prizer')
+        }
+        if (item.rise) {
+            iconThree.classList.add('main__icon-star_active')
+        }
+
+        name.id = "employee-name"
+        iconOne.id = "cookie"
+        iconTwo.id = "bin"
+        iconThree.id = "star"
+        iconOne.src = "/public/icons/cookie.png"
+        iconTwo.src = "/public/icons/bin.png"
+        iconThree.src = "/public/icons/star.png"
+        iconOne.alt = "icon"
+        iconTwo.alt = "icon"
+        iconThree.alt = "icon"
+        inp.type = "number"
+
+        name.innerHTML = item.name
+        inp.value = item.salary
+
+        main__item.append(name, inp, actions)
+        actions.append(iconOne, iconTwo, iconThree)
+        place.append(main__item)
+
+        iconOne.onclick = () => {
+            if (item.increase) {
+                item.increase = false
+                main__item.classList.remove('main__item_prizer')
+            } else {
+                item.increase = true
+                main__item.classList.add('main__item_prizer')
+            }
+        }
+
+        name.onclick = () => {
+            if (item.rise) {
+                item.rise = false
+                iconThree.classList.remove('main__icon-star_active')
+            } else {
+                item.rise = true
+                iconThree.classList.add('main__icon-star_active')
+            }
+        }
     }
 }
