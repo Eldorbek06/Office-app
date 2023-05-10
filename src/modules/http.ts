fa794f961798137fc8266628e1d701a4d529f66f
Eldor06 <eldorergashev20sm>
2023-05-10 15:36:04 +0500

    ts reset

--- src/modules/http.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const BASE_URL: string = import.meta.env.VITE_BASE_URL
-
-export const getData = async (path: string) => {
-    const res = await axios.get(BASE_URL + path)
-    
-    return res
-} 
